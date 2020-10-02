e8ec60d468fe0ec4fbc2a6348f9df58f906604f2
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-10-01 23:37:52 -0400

    Initial Setups are done

---
+++ github-user-card/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>GitHub User Card</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
